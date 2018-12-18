abaab3ad71d257874270f7bf0008ff5f29eef99f
Karol-Szykula <karol.szykula@wp.pl>
2018-12-18 20:14:24 +0100

    Add search input

--- src/List/List.js
+++ src/List/List.js
@@ -7,11 +7,12 @@ import './list.css'
 class List extends React.Component {
 
     state = {
-        users: []
+        users: [],
+        searchText: '',
     }
 
     componentDidMount() {
-        fetch(`https://randomuser.me/api/?results=${20}`)
+        fetch(`https://randomuser.me/api/?results=${100}`)
             .then(res => res.json())
             .then(users => {
                 console.dir(users.results)
@@ -19,23 +20,35 @@ class List extends React.Component {
             })
     }
 
+    searchInputTextChangeHandler = (event) => {
+        this.setState({ searchText: event.target.value })
+    }
+
     render() {
         return (
-            <ReactTable
-                data={this.state.users}
-                // resolveData={(data) => data.map((user) => (user))}
-                columns={[{
-                    Header: 'Title',
-                    accessor: 'name.title'
-                }, {
-                    Header: 'First Name',
-                    accessor: 'name.first'
-                }, {
-                    Header: 'Last Name',
-                    accessor: 'name.last'
-                }]}
-            />
+            <div className='List-list'>
+                <input
+                    name={'searchInput'}
+                    value={this.state.searchText}
+                    onChange={this.searchInputTextChangeHandler}
+                />
 
+                <ReactTable
+                    data={this.state.users}
+                    filterable={true}
+                    columns={[{
+                        Header: 'Title',
+                        accessor: 'name.title'
+                    }, {
+                        Header: 'First Name',
+                        accessor: 'name.first'
+                    }, {
+                        Header: 'Last Name',
+                        accessor: 'name.last'
+                    }]}
+                    defaultPageSize={5}
+                />
+            </div>
         )
     }
 }
