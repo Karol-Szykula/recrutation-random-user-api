cefb23802dd649b6967cd24ceb3fdf4af96a5eeb
Karol-Szykula <karol.szykula@wp.pl>
2018-12-19 15:36:26 +0100

    Add AboutUs component

---
+++ src/About-us/AboutUs.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AboutUs = (props) => (
+    <div>
+        AboutUs
+    </div>
+)
+
+export default AboutUs
--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import { store } from './store'
 import Navigation from './Navigation/Navigation'
 import List from './List/List'
 import NotFound from './Navigation/NotFound'
+import AboutUs from './About-us/AboutUs';
 
 class App extends Component {
   render() {
@@ -19,7 +20,9 @@ class App extends Component {
             <Route component={Navigation} />
             <Switch>
               <Route path='/' exact component={List} />
+              <Route path='/list' exact component={List} />
               <Route path='/:id([1-4])' component={List} />
+              <Route path='/aboutus' component={AboutUs} />
               <Route component={NotFound} />
             </Switch>
           </div>
