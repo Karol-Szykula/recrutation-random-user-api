decf978a930c85fa5a347c6ef05d39cad3e26692
Karol-Szykula <karol.szykula@wp.pl>
2018-12-20 20:24:33 +0100

    Add styles and description in AboutUs page

--- src/About-us/AboutUs.js
+++ src/About-us/AboutUs.js
@@ -3,8 +3,35 @@ import React from 'react'
 import './aboutus.css'
 
 const AboutUs = (props) => (
-    <div className='about-us'>
-        Our company offers:
+    <div className='about-us__wrapper'>
+        <div className="about-us">
+            Our company offers support in implementations of multi-channel eCommerce software.
+        </div>
+        <div class="about-us__item">
+            <div class="section-function__container-text">
+                B2C
+            </div>
+        </div>
+        <div class="about-us__item">
+            <div class="section-function__container-text">
+                CRM
+            </div>
+        </div>
+        <div class="about-us__item">
+            <div class="section-function__container-text">
+                B2B
+            </div>
+        </div>
+        <div class="about-us__item">
+            <div class="section-function__container-text">
+                PIM
+            </div>
+        </div>
+        <div class="about-us__item">
+            <div class="section-function__container-text">
+                Conversion and usability of existing systems
+            </div>
+        </div>
     </div>
 )
 
